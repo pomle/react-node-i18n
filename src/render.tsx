d67347b6a5d443ccbb9ad5c36a2f2bdba17cfd6f
Pontus Alexander <pontus.alexander@gmail.com>
2023-12-01 08:58:18 +0100

    Remove unused module

--- src/render.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { renderToString } from "react-dom/server";
-
-export function text(Component: React.ReactElement | string) {
-  const R = (Component as unknown) as (props: unknown) => JSX.Element;
-  return renderToString(<R />);
-}
